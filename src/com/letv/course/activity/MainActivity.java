ca3549f17bdecf9e06d44353592b8ee5526d7949
cheenlie <chenlinjsj087@gmail.com>
2015-05-29 18:51:25 +0800

    点击日期v1.0

--- src/com/letv/course/activity/MainActivity.java
+++ src/com/letv/course/activity/MainActivity.java
@@ -1,14 +1,23 @@
 package com.letv.course.activity;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.course.util.MyTimeUtil;
 import com.letv.course.R;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnShowListener;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -35,7 +44,53 @@ public class MainActivity extends Activity {
 		week = MyTimeUtil.getWeeks(currentDate);
 System.out.println(week);
 		main_topbar_spinner.setText(MyTimeUtil.getTheWeekStrMonthAndDay(week));
+		
+		main_topbar_spinner.setOnClickListener( (android.view.View.OnClickListener) new TextViewClickEvent());
+		
 
 	}
 
+	
+	public class TextViewClickEvent implements OnClickListener{
+
+		@Override
+		public void onClick(DialogInterface arg0, int arg1) {
+			
+			Calendar calendar=Calendar.getInstance();
+			SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy.MM.dd");
+			Date date=null;
+			
+			try {
+				date=simpleDateFormat.parse(MyTimeUtil.getDateOfWeekAndDay(week, 3));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			calendar.setTime(date);
+			
+			final DatePickerDialog datePickerDialog;
+			datePickerDialog=new DatePickerDialog(getApplicationContext(), new DatePickerDialog.OnDateSetListener() {
+				
+				@Override
+				public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+					
+				}
+			}, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+			
+			datePickerDialog.setOnShowListener(new OnShowListener() {
+				
+				public void onShow(DialogInterface arg0) {
+					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(datePickerDialog.getCurrentFocus().getWindowToken(),
+							InputMethodManager.HIDE_NOT_ALWAYS);					
+				}
+			});
+			
+			datePickerDialog.show();
+			
+		}
+		
+	}
+	
 }
